712363b3732f4533fda224d652d8545867a0d63a
Maciuska <Maciuska14@gmail.com>
2017-11-01 21:52:05 +0000

    app changes

--- server.js
+++ server.js
@@ -14,128 +14,133 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-var app = express();
-app.use(bodyParser.json());
+export default db => {
 
-// Defining data structures for companies and users in punchcard.com
-var companies = [];
-var users = [];
-var punches = new Map();
+	var app = express();
+	app.use(bodyParser.json());
 
-// Initialize listen for app to listen on a specific port, either provided or hardcoded
-app.listen(process.env.PORT || 5001);
+	// Defining data structures for companies and users in punchcard.com
+	var companies = [];
+	var users = [];
+	var punches = new Map();
 
-// Returns all companies, registered in the punchcard.com service
-app.get('/api/companies', function (req, res) {
-    return res.json(companies);
-});
+	// Initialize listen for app to listen on a specific port, either provided or hardcoded
+	app.listen(process.env.PORT || 5001);
 
-// Registers a new company to the punchcard.com service
-app.post('/api/companies', function (req, res) {
-    if (!req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('punchCount')) {
-        res.statusCode = 400;
-        return res.send('Post syntax error');
-    }
-    var newCompany = {
-        id: companies.length + 1,
-        name: req.body.name,
-        punchCount: req.body.punchCount
-    };
+	// Returns all companies, registered in the punchcard.com service
+	app.get('/api/companies', function (req, res) {
+		return res.json(companies);
+	});
 
-    companies.push(newCompany);
+	// Registers a new company to the punchcard.com service
+	app.post('/api/companies', function (req, res) {
+		if (!req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('punchCount')) {
+			res.statusCode = 400;
+			return res.send('Post syntax error');
+		}
+		var newCompany = {
+			id: companies.length + 1,
+			name: req.body.name,
+			punchCount: req.body.punchCount
+		};
 
-    res.json(true);
-});
+		companies.push(newCompany);
 
-// Gets a specific company, given a valid id
-app.get('/api/companies/:id', function (req, res) {
-    var company;
-    for (var i = 0; i < companies.length; i++) {
-        if (companies[i].id == req.params.id) {
-            company = companies[i];
-            break;
-        }
-    }
-    if (company) {
-        return res.json(company);
-    } else {
-        res.statusCode = 404;
-        return res.send('Company with given id was not found');
-    }
-});
+		res.json(true);
+	});
 
-// Gets all users in the system
-app.get('/api/users', function (req, res) {
-    return res.json(users);
-});
+	// Gets a specific company, given a valid id
+	app.get('/api/companies/:id', function (req, res) {
+		var company;
+		for (var i = 0; i < companies.length; i++) {
+			if (companies[i].id == req.params.id) {
+				company = companies[i];
+				break;
+			}
+		}
+		if (company) {
+			return res.json(company);
+		} else {
+			res.statusCode = 404;
+			return res.send('Company with given id was not found');
+		}
+	});
 
-// Creates a new user in the system
-app.post('/api/users', function (req, res) {
-    if (!req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('email')) {
-        res.statusCode = 400;
-        return res.send('User was not properly formatted');
-    }
-    var newUser = {
-        id: users.length + 1,
-        name: req.body.name,
-        email: req.body.email 
-    }
-    users.push(newUser);
+	// Gets all users in the system
+	app.get('/api/users', function (req, res) {
+		return res.json(users);
+	});
 
-    res.json(true);
-});
+	// Creates a new user in the system
+	app.post('/api/users', function (req, res) {
+		if (!req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('email')) {
+			res.statusCode = 400;
+			return res.send('User was not properly formatted');
+		}
+		var newUser = {
+			id: users.length + 1,
+			name: req.body.name,
+			email: req.body.email 
+		}
+		users.push(newUser);
 
-// Returns a list of all punches, registered for the given user
-app.get('/api/users/:id/punches', function (req, res) {
-    if (!isValidUser(req.params.id)) {
-        res.statusCode = 404;
-        return res.send('User with given id was not found in the system.');
-    }
-    // There was a ?company query provided
-    if (req.query.company) {
-        var filteredPunches = punches.get(req.params.id);
-        if (filteredPunches) {
-            // The user already has some punches in his list
-            var returnList = [];
-            filteredPunches.forEach(function (value, idx) {
-                if (value.companyId == req.query.company) {
-                    returnList.push(value);
-                }
-            });
-            return res.json(returnList);
-        } else {
-            return res.json([]);
-        }
-    } else {
-        var retrievedPunches = punches.get(req.params.id) === undefined ? [] : punches.get(req.params.id);
-        return res.json(retrievedPunches);
-    }
-});
+		res.json(true);
+	});
 
-// Creates a punch, associated with a user
-app.post('/api/users/:id/punches', function (req, res) {
-    if (!req.body.hasOwnProperty('companyId')) {
-        res.statusCode = 400;
-        return res.send('Company Id is missing');
-    } else if (!isValidUser(req.params.id)) {
-        res.statusCode = 404;
-        return res.send('The user was not found in the system.');
-    } else if (!isValidCompany(req.body.companyId)) {
-        res.statusCode = 404;
-        return res.send('The company with the given id was not found in the system.');
-    }
+	// Returns a list of all punches, registered for the given user
+	app.get('/api/users/:id/punches', function (req, res) {
+		if (!isValidUser(req.params.id)) {
+			res.statusCode = 404;
+			return res.send('User with given id was not found in the system.');
+		}
+		// There was a ?company query provided
+		if (req.query.company) {
+			var filteredPunches = punches.get(req.params.id);
+			if (filteredPunches) {
+				// The user already has some punches in his list
+				var returnList = [];
+				filteredPunches.forEach(function (value, idx) {
+					if (value.companyId == req.query.company) {
+						returnList.push(value);
+					}
+				});
+				return res.json(returnList);
+			} else {
+				return res.json([]);
+			}
+		} else {
+			var retrievedPunches = punches.get(req.params.id) === undefined ? [] : punches.get(req.params.id);
+			return res.json(retrievedPunches);
+		}
+	});
 
-    // We have valid data
-    var oldPunches = punches.get(req.params.id) === undefined ? [] : punches.get(req.params.id);
-    oldPunches.push({
-        companyId: req.body.companyId,
-        companyName: getCompanyNameById(req.body.companyId),
-        created: new Date().toLocaleString()
-    });
-    punches.set(req.params.id, oldPunches);
+	// Creates a punch, associated with a user
+	app.post('/api/users/:id/punches', function (req, res) {
+		if (!req.body.hasOwnProperty('companyId')) {
+			res.statusCode = 400;
+			return res.send('Company Id is missing');
+		} else if (!isValidUser(req.params.id)) {
+			res.statusCode = 404;
+			return res.send('The user was not found in the system.');
+		} else if (!isValidCompany(req.body.companyId)) {
+			res.statusCode = 404;
+			return res.send('The company with the given id was not found in the system.');
+		}
 
-    res.json(true);
-});
+		// We have valid data
+		var oldPunches = punches.get(req.params.id) === undefined ? [] : punches.get(req.params.id);
+		oldPunches.push({
+			companyId: req.body.companyId,
+			companyName: getCompanyNameById(req.body.companyId),
+			created: new Date().toLocaleString()
+		});
+		punches.set(req.params.id, oldPunches);
+
+		res.json(true);
+	});
+	
+	return app;
+};
 
 // Helper functions
 
