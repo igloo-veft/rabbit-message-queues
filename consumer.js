638174a3bb76d678ab764e34b537a038831632eb
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-11-01 22:48:28 +0000

    added more user event listeners

--- consumer.js
+++ consumer.js
@@ -1,11 +1,35 @@
 import createRabbitClient from "./createRabbitClient";
 
-const topic = "foo";
 createRabbitClient().then(ch => {
-  ch.assertQueue(topic).then(ok => {
-    return ch.consume(topic, msg => {
+  ch.assertQueue(addUserAction).then(ok => {
+    return ch.consume(addUserAction, msg => {
       if (msg !== null) {
-        console.log(msg.content.toString());
+        const data = JSON.parse(msg.content.toString());
+        console.log("User was added", data);
+        ch.ack(msg);
+      }
+    });
+  });
+});
+
+createRabbitClient().then(ch => {
+  ch.assertQueue(punchAction).then(ok => {
+    return ch.consume(punchAction, msg => {
+      if (msg !== null) {
+        const data = JSON.parse(msg.content.toString());
+        console.log("User got a punch", data);
+        ch.ack(msg);
+      }
+    });
+  });
+});
+
+createRabbitClient().then(ch => {
+  ch.assertQueue(discountAction).then(ok => {
+    return ch.consume(discountAction, msg => {
+      if (msg !== null) {
+        const data = JSON.parse(msg.content.toString());
+        console.log("User got discount", data);
         ch.ack(msg);
       }
     });
